5d1aab80cba37453981c8eb9729f3692e66e2602
kitweb <info@kitweb.pro>
2019-01-02 11:31:32 +0200

    ** refactor CORS in index.php (public folder)

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-header("Access-Control-Allow-Origin: http://localhost:8100");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
