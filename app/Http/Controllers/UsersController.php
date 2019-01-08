9cd3c69c1afe6e7a33fdaf554f3a5cdfb10a5f18
kitweb <info@kitweb.pro>
2019-01-08 12:36:52 +0200

    ** SetAvatar 500

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -184,7 +184,7 @@ class UsersController extends Controller
 
     public function SetAvatar(Request $request)
     {
-        return response()->json(['message' => 'Юра привет!'], 200);
+        return response()->json(['message' => 'Юра привет!'], 500);
         exit();
 
         $validator = Validator::make($request->all(), [
