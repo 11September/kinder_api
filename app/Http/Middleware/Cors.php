bf8afb75d70493bbb806639e01b616fef33f620b
kitweb <info@kitweb.pro>
2019-01-09 15:21:06 +0200

    ** update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,5 @@ class Cors
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorizations, Authorization, Content-Length, Accept, X-Requested-With, x-auth-token');
-//            ->header('Access-Control-Allow-Credentials: true');
     }
 }
