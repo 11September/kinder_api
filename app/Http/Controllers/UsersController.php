8d94894d7edb401819a9acaedb7d691e2052e6db
kitweb <info@kitweb.pro>
2019-01-10 17:36:28 +0200

    ** UsersController@set_avatar add necessary check for store base64. Add new responce if failed store.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -229,6 +229,11 @@ class UsersController extends Controller
         $folderPath = "images/uploads/avatars/";
         $folderPathSave = "/images/uploads/avatars/";
         $image_parts = explode(";base64,", $data);
+
+        if (!$image_parts){
+            return null;
+        }
+
         $image_type_aux = explode("image/", $image_parts[0]);
         $image_type = $image_type_aux[1];
         $image_base64 = base64_decode($image_parts[1]);
