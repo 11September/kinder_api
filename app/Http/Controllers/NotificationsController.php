f9468908fe5c91ef4af19a27e0467c7de235ecec
kitweb <info@kitweb.pro>
2019-01-15 14:28:24 +0200

    ** Try to send with message fix

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -51,7 +51,7 @@ class NotificationsController extends Controller
         $params = [];
         $params['include_player_ids'] = array("e903b975-66a8-4f32-a34e-587ad0c0bc40","1b96389f-51aa-4e8f-83a2-0f21ca926831");
         $params['contents'] = [
-            "ru" => $request->message
+            "en" => $request->message
         ];
 
         \OneSignal::sendNotificationCustom($params);
