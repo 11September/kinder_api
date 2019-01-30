eda84bbe4128c15024b22564fc3a3b35a5df2ef0
kitweb <info@kitweb.pro>
2019-01-30 12:28:54 +0200

    ** PostsController@index method add check to try property

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -28,7 +28,13 @@ class PostsController extends Controller
 
         try {
             $user = User::where('token', '=', $request->header('x-auth-token'))->first();
-            $group = $user->group()->first();
+
+//            $group = $user->group()->first();
+            if (!$user->group()->first()){
+                $group = Group::where('moderator_id', $user->id)->OrWhere('user_id', $user->id)->first();
+            }else{
+                $group = $user->group()->first();
+            }
 
             $posts = Post::select('id', 'title', 'body', 'preview', 'image')
                 ->where('until', '>=', date('Y-m-d'))
