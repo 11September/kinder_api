8e407aaafc371763040b96d9e11e26ff29532570
kitweb <info@kitweb.pro>
2019-01-31 14:42:46 +0200

    ** UsersController@login response @push@ status

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -57,6 +57,7 @@ class UsersController extends Controller
                         $result = array_add($result, 'parent_name', (isset($user->parent_name) ? $user->parent_name : null));
                         $result = array_add($result, 'group', (isset($group->name)) ? $group->name : null);
                         $result = array_add($result, 'avatar', $avatar);
+                        $result = array_add($result, 'push', $user->push);
                         $result = array_add($result, 'school_id', (isset($user->school_id)) ? $user->school_id : null);
                         $result = array_add($result, 'school_name', (isset($school->name) ? $school->name : null) );
 
