68622b584a1db5b351632db0a78a5e37e61490d8
kitweb <info@kitweb.pro>
2019-01-31 10:04:38 +0200

    ** login page => new page

--- resources/views/admin/template/master.blade.php
+++ resources/views/admin/template/master.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,14 +11,9 @@
     <title>Адміністративна панель "СадОк Медікавер"</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Bootstrap core CSS-->
     <link href="{{ asset('administrator/css/bootstrap.min.css') }}" rel="stylesheet">
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
           integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-
-    <!-- Custom styles for this template-->
     <link href="{{ asset('administrator/css/sb-admin.css') }}" rel="stylesheet">
 
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,170 @@
-@extends('layouts.app')
+{{--@extends('layouts.app')--}}
 
-@section('content')
+{{--@section('content')--}}
+    {{--<div class="container">--}}
+        {{--<div class="row justify-content-center">--}}
+            {{--<div class="col-md-8">--}}
+                {{--<div class="card">--}}
+                    {{--<div class="card-header">{{ __('Login') }}</div>--}}
+
+                    {{--<div class="card-body">--}}
+                        {{--<form method="POST" action="{{ route('login') }}">--}}
+                            {{--@csrf--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="email"--}}
+                                       {{--class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="email" type="email"--}}
+                                           {{--class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"--}}
+                                           {{--name="email" value="{{ old('email') }}" required autofocus>--}}
+
+                                    {{--@if ($errors->has('email'))--}}
+                                        {{--<span class="invalid-feedback" role="alert">--}}
+{{--<strong>{{ $errors->first('email') }}</strong>--}}
+{{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<label for="password"--}}
+                                       {{--class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
+
+                                {{--<div class="col-md-6">--}}
+                                    {{--<input id="password" type="password"--}}
+                                           {{--class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"--}}
+                                           {{--name="password" required>--}}
+
+                                    {{--@if ($errors->has('password'))--}}
+                                        {{--<span class="invalid-feedback" role="alert">--}}
+{{--<strong>{{ $errors->first('password') }}</strong>--}}
+{{--</span>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row">--}}
+                                {{--<div class="col-md-6 offset-md-4">--}}
+                                    {{--<div class="form-check">--}}
+                                        {{--<input class="form-check-input" type="checkbox" name="remember"--}}
+                                               {{--id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
+
+                                        {{--<label class="form-check-label" for="remember">--}}
+                                            {{--{{ __('Remember Me') }}--}}
+                                        {{--</label>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group row mb-0">--}}
+                                {{--<div class="col-md-8 offset-md-4">--}}
+                                    {{--<button type="submit" class="btn btn-primary">--}}
+                                        {{--{{ __('Login') }}--}}
+                                    {{--</button>--}}
+
+                                    {{--@if (Route::has('password.request'))--}}
+                                        {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
+                                            {{--{{ __('Forgot Your Password?') }}--}}
+                                        {{--</a>--}}
+                                    {{--@endif--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</form>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--@endsection--}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Адміністративна панель "СадОк Медікавер"</title>
+
+    <link href="{{ asset('administrator/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+          integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link href="{{ asset('administrator/css/sb-admin.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Вхід</div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input id="email" type="email"
+                               class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                               value="{{ old('email') }}" required autofocus placeholder="Адреса електронної пошти">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        <label for="email">Адреса електронної пошти</label>
+                    </div>
                 </div>
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input id="password" type="password"
+                               class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                               name="password" required placeholder="Пароль">
+
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+
+                        <label for="password">Пароль</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"
+                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            Запам'ятати пароль
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block" href="index.html">Вхід</button>
+            </form>
+            <div class="text-center">
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link login-link login-forgot-link" href="{{ route('password.request') }}">
+                        Забули пароль?
+                    </a>
+                @endif
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<script src="{{ asset('administrator/js/jquery.min.js') }}"></script>
+<script src="{{ asset('administrator/js/bootstrap.bundle.min.js') }}"></script>
+{{--<script src="{{ asset('administrator/js/sb-admin.js') }}"></script>--}}
+
+</body>
+</html>
+
