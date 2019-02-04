cf2ec53ef0db9de5404d8ee4f47f0a76124fd593
kitweb <info@kitweb.pro>
2019-02-04 14:55:29 +0200

    ** new method "message" from API

--- nodejs/server.js
+++ nodejs/server.js
@@ -16,34 +16,36 @@ redisClient.auth('password', function(err){
 
 redisClient.subscribe('message');
 
-redisClient.on("message", function(channel, data) {
-	var data = JSON.parse(data);
-	if(data.client_id in users){
-		if(data.conversation_id in users[data.client_id]){
-			users[data.client_id][data.conversation_id].emit("message", {"conversation_id":data.conversation_id,"message":data.message});
-		}
-	}
+redisClient.on("message", function (channel, data) {
+    var data = JSON.parse(data);
+    if (data.client_id in users) {
+        if (data.conversation_id in users[data.client_id]) {
+            users[data.client_id][data.conversation_id].emit("message", {
+                "conversation_id": data.conversation_id,
+                "message": data.message
+            });
+        }
+    }
 });
 
 io.on('connection', function (socket) {
+    socket.on("add user", function (data) {
+        if (!(data.client in users)) {
+            users[data.client] = {};
+        }
+        users[data.client][data.conversation] = socket;
+        socket.user_id = data.client;
+        socket.conversation_id = data.conversation;
+    });
 
-	socket.on("add user",function(data){
-    if(!(data.client in users)){
-      users[data.client] = {};
-    }
-    users[data.client][data.conversation]=socket;
-    socket.user_id = data.client;
-    socket.conversation_id = data.conversation;
-  });
+    socket.on('disconnect', function () {
+        if (!(socket.user_id in users)) return;
+        if (!(socket.conversation_id in users[socket.user_id])) return;
 
-  socket.on('disconnect', function() {
-    if(!(socket.user_id in users)) return;
-    if(!(socket.conversation_id in users[socket.user_id])) return;
-
-    delete users[socket.user_id][socket.conversation_id];
-    if(Object.keys(users[socket.user_id]).length === 0){
-      delete users[socket.user_id];
-    }
-  });
+        delete users[socket.user_id][socket.conversation_id];
+        if (Object.keys(users[socket.user_id]).length === 0) {
+            delete users[socket.user_id];
+        }
+    });
 
 });
