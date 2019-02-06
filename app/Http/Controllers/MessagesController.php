7951d78621d818c1e8c218f7e2ad9a7bae6fc409
kitweb <info@kitweb.pro>
2019-02-06 17:57:41 +0200

    ** Fix OneSignal if

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -57,7 +57,7 @@ class MessagesController extends Controller
             $redis = Redis::connection();
             $redis->publish('message', json_encode($data));
 
-            if (isset($user->player_id) && !empty($user->player_id)) {
+            if ($user && $user->player_id != null && isset($user->player_id) && !empty($user->player_id)) {
                 $this->sendToOneSignal($user, $request->message);
             }
 
@@ -185,7 +185,7 @@ class MessagesController extends Controller
                 ->active()
                 ->first();
 
-            if ($user && isset($user->player_id) && !empty($user->player_id)) {
+            if ($user && $user->player_id != null && isset($user->player_id) && !empty($user->player_id)) {
                 $user = Auth::user();
 
                 dd("in if", $user);
