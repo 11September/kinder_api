1923757435a1a611a4115b31280895417340978a
kitweb <info@kitweb.pro>
2019-02-08 11:26:41 +0200

    ** CheckSchoolGroupId middleware fix

---
+++ app/Http/Middleware/CheckSchoolGroupId.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckSchoolGroupId
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check()) {
+            if (Auth::user()->type == 'moderator' && !Auth::user()->school_id && !Auth::user()->group_id) {
+                return $next($request);
+            }
+        }
+        return redirect('admin')->with('error', 'Користувач не може брати участь в листуванні, так як він не прив\'язаний до жодної групи!');
+    }
+}
