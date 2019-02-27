96c019945286795ff9201a40efb754f9cb78b81d
kitweb <info@kitweb.pro>
2019-02-27 15:11:37 +0200

    Fix middleware login to admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function admin()
     {
         $schools = School::count();
