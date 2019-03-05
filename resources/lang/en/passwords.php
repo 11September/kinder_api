5eb424616beca61e25631023563aed51defa9676
kitweb <info@kitweb.pro>
2019-03-05 11:00:22 +0200

    ** welcome blade ** uk errors ** 404 blade

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Паролі повинні містити принаймні шість символів та відповідати підтвердженню.',
+    'reset' => 'Ваш пароль скинутий!',
+    'sent' => 'Ми надіслали електронною поштою ваше посилання для скидання пароля!',
+    'token' => 'Цей маркер скидання пароля недійсний.',
+    'user' => "Ми не можемо знайти користувача з цією адресою електронної пошти.",
 
 ];
